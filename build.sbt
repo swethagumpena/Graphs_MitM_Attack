f1947ab2546d10a6633e7f6c159b19bd4f70abee
swetha gumpena <swetha.gumpena0607@gmail.com>
2023-11-01 20:46:01 -0500

    chore: setup logger and spark

--- build.sbt
+++ build.sbt
@@ -4,5 +4,14 @@ ThisBuild / scalaVersion := "2.13.12"
 
 lazy val root = (project in file("."))
   .settings(
-    name := "Graphs_MitM_Attack"
+    name := "Graphs_MitM_Attack",
+    libraryDependencies ++= Seq(
+      "org.slf4j" % "slf4j-api" % "2.0.5",
+      "ch.qos.logback" % "logback-classic" % "1.4.7",
+      "org.apache.spark" %% "spark-core" % "3.4.1",
+      "org.apache.spark" %% "spark-sql" % "3.4.1",
+      "org.apache.spark" %% "spark-graphx" % "3.4.1",
+      "org.scalatest" %% "scalatest" % "3.0.8" % "test",
+      "com.typesafe" % "config" % "1.4.1",
+    )
   )
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,21 @@
+import org.slf4j.LoggerFactory
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.sql.SparkSession
+
 object Main {
+  private val logger = LoggerFactory.getLogger(getClass)
+
   def main(args: Array[String]): Unit = {
-    println("Hello world!")
+    logger.info("In spark application")
+
+    val isRunningOnAWS = sys.env.contains("AWS_REGION") || sys.env.contains("AWS_EXECUTION_ENV")
+
+    val sc = if (isRunningOnAWS) {
+      val spark = SparkSession.builder().appName("Graphs_MitM_Attack").getOrCreate()
+      spark.sparkContext
+    } else {
+      val conf = new SparkConf().setAppName("RandomWalksApp").setMaster("local[4]") // Set master to local with 4 cores
+      new SparkContext(conf)
+    }
   }
 }
